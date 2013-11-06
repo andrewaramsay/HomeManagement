1abe53c31cdc98a29a01ba369c24b46336ba85b8
Andrew Ramsay <aramsaybusiness@gmail.com>
2013-11-06 07:01:30 -0500

    -Added CDN support for jquery, jquery-ui, and jquerymobile

--- HomeManagementMobile/App_Start/BundleConfig.cs
+++ HomeManagementMobile/App_Start/BundleConfig.cs
@@ -8,10 +8,17 @@ namespace HomeManagementMobile
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.UseCdn = true;
+            var jqueryCdnPath = "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.1.min.js";
+            var jqueryUiCdnPath = "http://ajax.aspnetcdn.com/ajax/jquery.ui/1.10.3/jquery-ui.min.js";
+            var jqueryUiCssCdnPath = "http://ajax.aspnetcdn.com/ajax/jquery.ui/1.10.3/themes/excite-bike/jquery-ui.css";
+            var jqueryMobileCdnPath = "http://ajax.aspnetcdn.com/ajax/jquery.mobile/1.3.2/jquery.mobile-1.3.2.min.js";
+            var jqueryMobileCssCdnPath = "http://ajax.aspnetcdn.com/ajax/jquery.mobile/1.3.2/jquery.mobile-1.3.2.min.css";
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery", jqueryCdnPath).Include(
                         "~/Scripts/jquery-1.*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui", jqueryUiCdnPath).Include(
                         "~/Scripts/jquery-ui*"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -21,13 +28,13 @@ namespace HomeManagementMobile
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jquerymobile").Include("~/Scripts/jquery.mobile*"));
+            bundles.Add(new ScriptBundle("~/bundles/jquerymobile", jqueryMobileCdnPath).Include("~/Scripts/jquery.mobile*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
-            bundles.Add(new StyleBundle("~/Content/mobilecss").Include("~/Content/jquery.mobile*"));
+            bundles.Add(new StyleBundle("~/Content/mobilecss", jqueryMobileCssCdnPath).Include("~/Content/jquery.mobile*"));
 
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+            bundles.Add(new StyleBundle("~/Content/themes/base/css", jqueryUiCssCdnPath).Include(
                         "~/Content/themes/base/jquery.ui.core.css",
                         "~/Content/themes/base/jquery.ui.resizable.css",
                         "~/Content/themes/base/jquery.ui.selectable.css",
